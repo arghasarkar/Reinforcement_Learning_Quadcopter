550a54291ff12a1b93c92132ad5074b069aa83a4
Argha Sarkar <argha.sarkar1994@gmail.com>
2018-09-14 18:05:30 +0100

    WIP

--- agents/DDPG.py
+++ agents/DDPG.py
@@ -28,9 +28,9 @@ class DDPG():
         self.actor_target.model.set_weights(self.actor_local.model.get_weights())
 
         # Noise process
-        self.exploration_mu = 0.1
-        self.exploration_theta = 0.20
-        self.exploration_sigma = 0.12
+        self.exploration_mu = 0.2
+        self.exploration_theta = 0.35
+        self.exploration_sigma = 0.4
         self.noise = OUNoise(self.action_size, self.exploration_mu, self.exploration_theta, self.exploration_sigma)
 
         # Replay memory
@@ -39,8 +39,8 @@ class DDPG():
         self.memory = ReplayBuffer(self.buffer_size, self.batch_size)
 
         # Algorithm parameters
-        self.gamma = 0.95  # discount factor
-        self.tau = 0.02  # for soft update of target parameters
+        self.gamma = 0.90  # discount factor
+        self.tau = 0.0005  # for soft update of target parameters
         
         # Score tracker 
         self.score = 0
